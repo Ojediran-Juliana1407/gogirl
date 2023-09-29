acb7b02a0afecca8513547534d21869e1c90c814
Ojediran Juliana <ojediranjuliana9@gmail.com>
2023-09-29 19:13:59 +0100

    color

---
+++ index.js
@@ -0,0 +1,23 @@
+let people = document.getElementById("downHead")
+console.log(people)
+
+let people1 = document.getElementsByClassName("septa")
+console.log(people1[1])
+
+let paragraph = document.querySelectorAll("p") 
+console.log(paragraph)
+
+let paragraph2 = document.querySelector(".septa")
+console.log(paragraph2)
+
+let peg = document.getElementById("btn")
+console.log(peg)
+getButton.addEventListener("mouseover", function (){
+    alert("please take me")
+})
+
+let textdom = document.getElementById("text")
+console.log(text)
+
+let divtag = document.getElementById("textnew")
+let newtext = document.createElement("h1")
